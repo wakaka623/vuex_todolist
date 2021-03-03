62b2e05926c8d5907ec558bd223289aac2fecd36
wakaka <568123086@qq.com>
2021-03-03 15:22:51 +0800

    完成todolist

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,101 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-input placeholder="请输入任务" class="my_ipt" :value="inputValue" @change="handleInputChange" />
+    <a-button type="primary"  @click="addItemToList">添加事项</a-button>
+
+    <a-list bordered :dataSource="list" class="dt_list">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <!-- 复选框 -->
+        <a-checkbox :checked="item.done" @change="cbStateChanged(item.id,$event)">{{item.info}}</a-checkbox>
+        <!-- 删除链接 -->
+        <a slot="actions" @click="removeItemById(item.id)">删除</a>
+      </a-list-item>
+
+      <!-- footer区域 -->
+      <div slot="footer" class="footer">
+        <!-- 未完成的任务个数 -->
+        <span>{{unDoneLength}}条剩余</span>
+        <!-- 操作按钮 -->
+        <a-button-group>
+          <a-button :type="viewKey ==='all'?'primary':'default'" @click="changeList('all')">全部</a-button>
+          <a-button :type="viewKey ==='undone'?'primary':'default'" @click="changeList('undone')">未完成</a-button>
+          <a-button :type="viewKey ==='done'?'primary':'default'" @click="changeList('done')">已完成</a-button>
+        </a-button-group>
+        <!-- 把已经完成的任务清空 -->
+        <a @click="clean">清除已完成</a>
+      </div>
+    </a-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapState,mapGetters } from 'vuex'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      // list:[]
+    }
+  },
+  created(){
+    console.log(this.$store)
+    this.$store.dispatch('getList')
+  },
+  methods:{
+    handleInputChange(e){
+      console.log(e.target.value);
+      this.$store.commit('setInputValue',e.target.value)
+    },
+    addItemToList(){
+      if(this.inputValue.trim()<=0) return this.$message.warning('别闹！啥都没输写，不干了！')
+      this.$store.commit('addItem')
+    },
+    removeItemById(id){
+      console.log(id);
+      this.$store.commit('removeItem',id)
+    },
+    cbStateChanged(id,e){
+      const param={
+        id:id,
+        status:e.target.checked
+      }
+      this.$store.commit('changeStatus',param)
+    },
+    clean(){
+      this.$store.commit('cleanDone')
+    },
+    changeList( key ){
+    //点击“全部”，“已完成”，“未完成”时触发
+    this.$store.commit('changeKey',key)
+    }
+  },
+  computed:{
+    ...mapState(['list','inputValue','viewKey']),
+    ...mapGetters(['unDoneLength','infoList'])
   }
+ 
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 10px;
+}
+
+.my_ipt {
+  width: 500px;
+  margin-right: 10px;
+}
+
+.dt_list {
+  width: 500px;
+  margin-top: 10px;
+}
+
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,85 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    //所有任务列表
+    list:[],
+    //文本输入框中的值
+    inputValue:'AAA',
+     //保存默认的选项卡值
+    viewKey:'all'
+
   },
   mutations: {
+    initList(state,list){
+      state.list=list
+    },
+    setInputValue(state,value){
+      state.inputValue=value
+    },
+    //添加列表项
+    addItem(state){
+      const obj={
+        id:state.nextId,
+        info:state.inputValue.trim(),
+        done:false
+      }
+      //将创建好的事项添加到数组list中
+      state.list.push(obj)
+      //nextId值自增
+      state.nextId++
+      state.inputValue=''
+    },
+    //删除列表数据
+    removeItem(state,id){
+      const index=state.list.findIndex(x=>x.id===id)
+      console.log(index)
+      if(index!=-1) state.list.splice(index,1)
+    },
+    changeStatus(state,param){
+      //根据id改变对应事项的状态
+      const index=state.list.findIndex(x=>x.id===param.id)
+      console.log(index)
+      if(index!=-1) state.list[index].done=param.status
+    },
+    cleanDone(state){
+      state.list = state.list.filter( x => x.done === false )
+    },
+    changeKey(state,key){
+      //当用户点击“全部”，“已完成”，“未完成”选项卡时触发
+      state.viewKey = key
+    }
   },
   actions: {
+    getList(context){
+      axios.get('./list.json').then(({data})=>{
+        console.log(data);
+        context.commit('initList',data)
+      })
+    }
   },
   modules: {
+  },
+  getters:{
+    unDoneLength(state){
+      const temp = state.list.filter( x => x.done === false )
+      console.log(temp)
+      return temp.length
+    }
+  },
+  infoList(state){
+    if(state.viewKey === 'all'){
+      return state.list
+    }
+    if(state.viewKey === 'undone'){
+      return state.list.filter( x => x.done === false )
+    }
+    if(state.viewKey === 'done'){
+      return state.list.filter( x => x.done === true )
+    }
   }
 })
